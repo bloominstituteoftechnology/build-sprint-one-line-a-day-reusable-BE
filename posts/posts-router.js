a0dd5d60daa4a0a13b2e7ba43f65866a7615c06d
Wayne Mitt <wmitt85@gmail.com>
2019-09-24 20:05:21 -0500

    added endpoints for updating and deleting posts

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -6,32 +6,6 @@ const router = express.Router();
 
 const restricted = require('../auth/restricted-middleware.js');
 
-// router.get('/', (req, res) => {
-//   Posts.find()
-//   .then(posts => {
-//     res.json(posts);
-//   })
-//   .catch(err => {
-//     res.status(500).json({ message: 'Failed to get posts' });
-//   });
-// });
-
-// router.get('/:id', (req, res) => {
-//   const { id } = req.params;
-
-//   Posts.findById(id)
-//   .then(scheme => {
-//     if (scheme) {
-//       res.json(scheme);
-//     } else {
-//       res.status(404).json({ message: 'Could not find scheme with given id.' })
-//     }
-//   })
-//   .catch(err => {
-//     res.status(500).json({ message: 'Failed to get Posts' });
-//   });
-// });
-
 router.get('/:id/posts', restricted, (req, res) => {
   const { id } = req.params;
 
@@ -66,47 +40,27 @@ router.post('/:id/posts', restricted, (req, res) => {
   });
 });
 
-router.post('/:id/steps', (req, res) => {
-  const stepData = req.body;
-  const { id } = req.params; 
-
-  Posts.findById(id)
-  .then(scheme => {
-    if (scheme) {
-      Posts.addStep(stepData, id)
-      .then(step => {
-        res.status(201).json(step);
-      })
-    } else {
-      res.status(404).json({ message: 'Could not find scheme with given id.' })
-    }
-  })
-  .catch (err => {
-    res.status(500).json({ message: 'Failed to create new step' });
-  });
-});
-
-router.put('/:id', (req, res) => {
+router.put('/posts/:id', (req, res) => {
   const { id } = req.params;
   const changes = req.body;
 
   Posts.findById(id)
-  .then(scheme => {
-    if (scheme) {
+  .then(post => {
+    if (post) {
       Posts.update(changes, id)
-      .then(updatedScheme => {
-        res.json(updatedScheme);
+      .then(updatedPost => {
+        res.json(updatedPost);
       });
     } else {
-      res.status(404).json({ message: 'Could not find scheme with given id' });
+      res.status(404).json({ message: 'Could not find post with given id' });
     }
   })
   .catch (err => {
-    res.status(500).json({ message: 'Failed to update scheme' });
+    res.status(500).json({ message: 'Failed to update post' });
   });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/posts/:id', (req, res) => {
   const { id } = req.params;
 
   Posts.remove(id)
@@ -114,11 +68,11 @@ router.delete('/:id', (req, res) => {
     if (deleted) {
       res.json({ removed: deleted });
     } else {
-      res.status(404).json({ message: 'Could not find scheme with given id' });
+      res.status(404).json({ message: 'Could not find post with given id' });
     }
   })
   .catch(err => {
-    res.status(500).json({ message: 'Failed to delete scheme' });
+    res.status(500).json({ message: 'Failed to delete post' });
   });
 });
 
